a4af82eadd3b593f3b0193950df18ebff3f09ad8
André Lucas <andrelrs.v@outlook.com>
2020-08-25 18:50:11 -0300

    aula 17, handle exception

--- src/main/java/academy/devdojo/springboot2/handler/RestExceptionHandler.java
+++ src/main/java/academy/devdojo/springboot2/handler/RestExceptionHandler.java
@@ -2,29 +2,53 @@ package academy.devdojo.springboot2.handler;
 
 import academy.devdojo.springboot2.exception.ResourceNotFoundDetails;
 import academy.devdojo.springboot2.exception.ResourceNotFoundException;
+import academy.devdojo.springboot2.exception.ValidationExceptionDetails;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
+@Slf4j
 public class RestExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ResourceNotFoundDetails> handleResourceNotFoundException(ResourceNotFoundException resourceNotFoundException) {
-        ResourceNotFoundDetails response = getResourceResponse(resourceNotFoundException);
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-    }
-
-    private ResourceNotFoundDetails getResourceResponse(ResourceNotFoundException resourceNotFoundException) {
-        return ResourceNotFoundDetails.builder()
+    public ResponseEntity<ResourceNotFoundDetails> handleResourceNotFoundException(ResourceNotFoundException exception) {
+        ResourceNotFoundDetails response = ResourceNotFoundDetails.builder()
                 .timestamp(LocalDateTime.now())
                 .status(HttpStatus.NOT_FOUND.value())
                 .title("Resource not found")
-                .detail(resourceNotFoundException.getMessage())
-                .developerMessage(resourceNotFoundException.getClass().getName())
+                .detail(exception.getMessage())
+                .developerMessage(exception.getClass().getName())
                 .build();
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationExceptionDetails> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+
+        String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
+        String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
+
+        ValidationExceptionDetails response = ValidationExceptionDetails.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .title("Fields validation error")
+                .detail("check the field(s)")
+                .developerMessage(exception.getClass().getName())
+                .fields(fields)
+                .fieldsMessage(fieldsMessage)
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
