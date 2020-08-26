c06c81004389740a079d40f4df0fdb8560fbca0c
André Lucas <andrelrs.v@outlook.com>
2020-08-26 14:44:29 -0300

    aula 25, rest template

--- src/main/java/academy/devdojo/springboot2/client/SpringClient.java
+++ src/main/java/academy/devdojo/springboot2/client/SpringClient.java
@@ -4,8 +4,7 @@ import academy.devdojo.springboot2.domain.Anime;
 import academy.devdojo.springboot2.wrapper.PageableResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -13,23 +12,54 @@ import java.util.List;
 
 @Slf4j
 public class SpringClient {
+    
+    private static final String URL = "http://localhost:8080/animes";
+    
     public static void main(String[] args) {
-        ResponseEntity<Anime> animeResponseEntity = new RestTemplate().getForEntity("http://localhost:8080/animes/{id}", Anime.class, 1);
+//        testGetWithRestTemplate();
+
+        ResponseEntity<PageableResponse<Anime>> animeListPage = new RestTemplate().exchange(URL, HttpMethod.GET, null, new ParameterizedTypeReference<PageableResponse<Anime>>() {
+        });
+        log.info("Anime List{} ", animeListPage.getBody());
+
+//        Anime anime = Anime
+//                .builder()
+//                .name("Attack on Titan")
+//                .build();
+//
+//        Anime animeSave = new RestTemplate().postForObject(URL, anime, Anime.class);
+//
+//        log.info("Anime Save{} ", animeSave);
+
+        Anime animeCowboy = Anime
+                .builder()
+                .name("Cowboy Bebop")
+                .build();
+
+        ResponseEntity<Anime> animeResponse = new RestTemplate().exchange(URL, HttpMethod.POST, new HttpEntity<>(animeCowboy, createJsonHeader()), Anime.class);
+        log.info("Anime SaveResponse{} ", animeResponse.getBody());
+    }
+
+    private static HttpHeaders createJsonHeader() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+//        httpHeaders.setBearerAuth("token");
+        return httpHeaders;
+    }
+
+    private static void testGetWithRestTemplate() {
+        ResponseEntity<Anime> animeResponseEntity = new RestTemplate().getForEntity(URL+"/{id}", Anime.class, 1);
         log.info("Response Entity {} ", animeResponseEntity);
         log.info("Response Data {} ", animeResponseEntity.getBody());
 
-        Anime anime = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class, 1);
+        Anime anime = new RestTemplate().getForObject(URL+"/{id}", Anime.class, 1);
         log.info("Anime {} ", anime);
 
-        Anime[] animeArray = new RestTemplate().getForObject("http://localhost:8080/animes/list", Anime[].class);
+        Anime[] animeArray = new RestTemplate().getForObject(URL+"/list", Anime[].class);
         log.info("Anime Array{} ", Arrays.toString(animeArray));
 
-        ResponseEntity<List<Anime>> animeList = new RestTemplate().exchange("http://localhost:8080/animes/list", HttpMethod.GET, null, new ParameterizedTypeReference<List<Anime>>() {
+        ResponseEntity<List<Anime>> animeList = new RestTemplate().exchange(URL+"/list", HttpMethod.GET, null, new ParameterizedTypeReference<List<Anime>>() {
         });
         log.info("Anime List{} ", animeList.getBody());
-
-        ResponseEntity<PageableResponse<Anime>> animeListPage = new RestTemplate().exchange("http://localhost:8080/animes", HttpMethod.GET, null, new ParameterizedTypeReference<PageableResponse<Anime>>() {
-        });
-        log.info("Anime List{} ", animeListPage.getBody());
     }
 }
