c879f2cf7c6942230e885b2936d300545953ea0b
André Lucas <andrelrs.v@outlook.com>
2020-08-28 11:11:14 -0300

    aula 39, spring actuator

---
+++ src/main/resources/init.sql
@@ -0,0 +1,5 @@
+INSERT INTO anime.dev_user (id, authorities, name, password, username)
+VALUES (default, 'ROLE_USER,ROLE_ADMIN','André Lucas', '{bcrypt}$2a$10$3WYoUm1SvwGnJOWHlK99m.kXBsSSSGvG7pH8n7OLNa/WBmaBdOXjW', 'andre')
+
+INSERT INTO anime.dev_user (id, authorities, name, password, username)
+VALUES (default, 'ROLE_USER','Dev', '{bcrypt}$2a$10$3WYoUm1SvwGnJOWHlK99m.kXBsSSSGvG7pH8n7OLNa/WBmaBdOXjW', 'dev')
