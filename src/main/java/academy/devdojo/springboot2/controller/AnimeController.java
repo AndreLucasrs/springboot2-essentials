32bcaeb588c151344b37a70f12d4e89266b1193a
André Lucas <andrelrs.v@outlook.com>
2020-08-27 23:26:12 -0300

    aula 38, springdoc openAPI

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -2,13 +2,15 @@ package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
 import academy.devdojo.springboot2.service.AnimeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +27,9 @@ public class AnimeController {
     private final AnimeService service;
 
     @GetMapping
+    @Operation(summary = "Lista de todos os animes paginas e ordenados",
+    description = "Usar paginação e ordenação basta adicionar os parametros ?page='number'&sort='campo' na url",
+    tags = {"anime"})
     public ResponseEntity<Page<Anime>> listaAll(Pageable pageable) {
 
         return ResponseEntity.ok(service.listAll(pageable));
@@ -59,6 +64,10 @@ public class AnimeController {
     }
 
     @DeleteMapping(value = "/admin/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Sucesso, dado removido"),
+            @ApiResponse(responseCode = "404", description = "dado não encontrado")
+    })
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
--- src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -1,5 +1,6 @@
 package academy.devdojo.springboot2.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,5 +23,6 @@ public class Anime {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotBlank(message = "O nome do anime é obrigatorio")
+    @Schema(description = "Nome do anime", example = "Cowbow bebop", required = true)
     private String name;
 }
